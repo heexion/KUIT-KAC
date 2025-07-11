f43b930bfc8f8da6355d430757a01cc4de19b92d
heexion <alsgml030901@naver.com>
2025-07-11 21:24:55 +0900

    feat: 식단 검색 및 상세 기록 화면 구현

---
+++ app/src/main/java/com/konkuk/kuit_kac/presentation/diet/FoodInfo.kt
@@ -0,0 +1,20 @@
+package com.konkuk.kuit_kac.presentation.diet
+
+import com.konkuk.kuit_kac.R
+
+data class FoodInfo(
+    val image: Int,
+    val carbohydrate: Float,
+    val protein: Float,
+    val fat: Float,
+    val calories: Int,
+    val unitWeight: Int
+)
+
+val foodInfoMap = mapOf(
+    "마라탕" to FoodInfo(R.drawable.ic_stew_hotpot, 5.2f, 3.0f, 4.1f, 350, 500),
+    "파스타" to FoodInfo(R.drawable.ic_noodles, 20.5f, 6.0f, 7.2f, 480, 300),
+    "족발" to FoodInfo(R.drawable.ic_meat, 0.5f, 15.0f, 10.0f, 290, 150),
+    "돼지고기 김치찌개" to FoodInfo(R.drawable.ic_stew_hotpot, 6.4f, 8.2f, 5.5f, 210, 250)
+    // 계속 추가...
+)
