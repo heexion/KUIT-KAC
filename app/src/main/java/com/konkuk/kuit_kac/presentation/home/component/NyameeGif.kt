fbe9f7e868151f0a398852cecaf79744921aad75
Bb-jh <203391596+Bb-jh@users.noreply.github.com>
2025-08-02 01:25:08 +0900

    feat: 냠이 애니메이션 컴포넌트 수정

--- app/src/main/java/com/konkuk/kuit_kac/presentation/home/component/NyameeGif.kt
+++ app/src/main/java/com/konkuk/kuit_kac/presentation/home/component/NyameeGif.kt
@@ -15,9 +15,11 @@ import kotlin.random.Random
 fun NyameeGif(
     modifier: Modifier = Modifier,
     onClick: () -> Unit = {},
-    num: Int = 1
+    num: Int = 1,
+    sizePercent: Float = 1f
 ) {
 //    val randNum = Random.nextInt(3) + 1
+
     Box(
         modifier = modifier
     ) {
@@ -25,32 +27,33 @@ fun NyameeGif(
             // 1: 팔굽혀펴기 2: 헤드셋 3: 덤벨
             1 -> GifImage(
                 modifier = Modifier
-                    .size(375.2f.wp(), 488f.bhp())
-                    .offset(x = 0f.wp(), y = 0f.hp()),
+//                    .size(375.2f.wp(), 488f.bhp())
+                    .offset(x = 0f.wp(), y = 0f.bhp()),
                 drawableId = R.drawable.gif_nyamee_jumpingjacks_effect,
                 onClick = { onClick() },
                 actualWidth = 187.6f.wp(),
-                actualHeight = 244f.bhp()
+                actualHeight = 244f.bhp(),
+                size = sizePercent
             )
 
             2 -> GifImage(
                 modifier = Modifier
-                    .size(260.16f.wp(), 360.4f.bhp())
-                    .offset(x = 55f.wp(), y = 74f.hp()),
+//                    .size(260.16f.wp(), 360.4f.bhp())
+                    .offset(x = -5f.wp(), y = 10f.bhp()),
                 drawableId = R.drawable.gif_nyamee_headset,
                 onClick = { onClick() },
                 actualWidth = 187.6f.wp(),
-                actualHeight = 244f.bhp()
+                actualHeight = 244f.bhp(), size = sizePercent * 0.69f
             )
 
             3 -> GifImage(
-                modifier = Modifier
-                    .size(324.2f.wp(), 400f.bhp())
-                    .offset(x = 25f.wp(), y = 29f.hp()),
+                modifier = Modifier,
+//                    .size(324.2f.wp(), 400f.bhp())
+//                    .offset(x = 25f.wp(), y = 29f.hp()),
                 drawableId = R.drawable.gif_nyamee_dumbbell,
                 onClick = { onClick() },
                 actualWidth = 187.6f.wp(),
-                actualHeight = 244f.bhp()
+                actualHeight = 244f.bhp(), size = sizePercent*0.86f
             )
         }
     }
