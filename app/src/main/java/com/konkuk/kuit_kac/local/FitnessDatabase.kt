9c0e8a7f8e1e1224e1270c5042073d9153b26e76
siusiubeom <siubeom200591ugmaie.com>
2025-08-21 04:41:40 +0900

    db 연결

--- app/src/main/java/com/konkuk/kuit_kac/local/FitnessDatabase.kt
+++ app/src/main/java/com/konkuk/kuit_kac/local/FitnessDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.konkuk.kuit_kac.local.dao.FitnessDao
 import com.konkuk.kuit_kac.local.dao.FoodDao
 
-@Database(entities = [Fitness::class], version = 5, exportSchema = false)
+@Database(entities = [Fitness::class], version = 6, exportSchema = false)
 abstract class FitnessDatabase : RoomDatabase() {
     abstract fun fitnessDao(): FitnessDao
 
